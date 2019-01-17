4ed6a9ef4d9f8365fd52497bcfea7839dc2176f7
graupv <pin18848@uvg.edu.gt>
2019-01-17 13:28:26 -0600

    cambios a MyRadio y a MyRadioTest
    - Agregar cuerpo a metodos de subir/bajar, encender.
    - crear metodo apagar.
    
    Falta:
    - 2 preubas unitarias
    - metodos de MyRadio:
    -- set y get favorito

--- MyRadio/src/MyRadio.java
+++ MyRadio/src/MyRadio.java
@@ -43,13 +43,20 @@ class Rad implements iRadio{
 		this.encendido = false;
 		this.frecuenciaActual = 87.9;
 	}
+
 	
 
     public boolean encendidoRadio() {
     	
-    	return true;
+    	if (this.encendido) {
+    		System.out.println("Ya esta encendido!\n");
+    		return true;
+    	} else {
+    		this.encendido = true;
+    		return true;
+    	}
+    	
     }
-    
     
     public double subirFrecuencia() {
     	/*
@@ -58,13 +65,51 @@ class Rad implements iRadio{
     	 *  3. else subir actual + 0.2
     	 *  
     	 * 
-    	 * */
-    	 
-    	return 3.3;
+    	 */
+    	if (this.fm) {
+    		//	true significa estamos en FM
+    		if (this.frecuenciaActual == 107.9) {
+    			this.frecuenciaActual = 87.9;
+    			return 87.9;
+    		} else {
+    			this.frecuenciaActual += 0.2;
+    			return this.frecuenciaActual;
+    		}
+    		
+    	} else {
+    		// false entonces estamos en AM
+    		if (this.frecuenciaActual == 1610) {
+    			this.frecuenciaActual = 530;
+    			return 530;
+    		} else {
+    			this.frecuenciaActual += 10;
+    			return this.frecuenciaActual;
+    		}
+    	}
+    	
     }
     
     public double bajarFrecuencia() {
-    	return 3.3;
+    	if (this.fm) {
+    		//	true significa estamos en FM
+    		if (this.frecuenciaActual == 87.9) {
+    			this.frecuenciaActual = 107.9;
+    			return 107.9;
+    		} else {
+    			this.frecuenciaActual -= 0.2;
+    			return this.frecuenciaActual;
+    		}
+    		
+    	} else {
+    		// false entonces estamos en AM
+    		if (this.frecuenciaActual == 530) {
+    			this.frecuenciaActual = 1610;
+    			return 1610;
+    		} else {
+    			this.frecuenciaActual -= 10;
+    			return this.frecuenciaActual;
+    		}
+    	}
     	
     }
     
@@ -80,7 +125,26 @@ class Rad implements iRadio{
     
 
     public boolean cambiarAmFm() {
-    	return true;
+    	if (this.fm) {
+    		// FM -> AM
+    		this.fm = false;
+    		this.frecuenciaActual = 530;
+    		return true;
+    	} else {
+    		// AM -> FM
+    		this.fm = true;
+    		this.frecuenciaActual = 87.9;
+    		return true;
+    	}
+    }
+    
+    public void apagar() {
+    	if (this.encendido) {
+    		this.encendido = false;
+    		
+    	} else {
+    		System.out.println("Ya esta apagado!\n");
+    	}
     }
     
 	
--- MyRadio/src/MyRadioTest.java
+++ MyRadio/src/MyRadioTest.java
@@ -7,12 +7,27 @@ import org.junit.Test;
 public class MyRadioTest {
 
 	@Test
-	public void testEquals() {
+	public void test_de_subir_frecuencia() {
 		Rad laRadio = new Rad();
 		double res = laRadio.bajarFrecuencia();
 //		assertEquals(3.3, res);
 //		assertCondition(ExcpetedValue, testedValue)
 //		assertCondition(ExcpetedArray, testedArray)
+		double frec = 88.1;
+		assertEquals(frec, laRadio.frecuenciaActual);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void test_de_guardar_favorito() {
+		Rad laRadio = new Rad();
+		double[] arr_test = new double[12];
+		arr_test[5] = 88.9;
+		
+		laRadio.setFavorito(5);
+		// assertArrayEquals("Expected value", "Tested value");
+		//	verificando que el valor se guardo en la misma posicion
+		assertEquals(arr_test[5], laRadio.favoritos[5]);
+		
 	}
 
 }
